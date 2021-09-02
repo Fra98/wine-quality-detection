d3b27906063d2de6a0af60f3dc13a0da34c1d4c7
Francesco Torta <62566275+Fra98@users.noreply.github.com>
2021-09-02 16:31:38 +0200

    Improvements to LDA: - added direction's orthogonalization - removed unnecessary multiclass handling

--- src/LDA.py
+++ src/LDA.py
@@ -3,37 +3,39 @@ import scipy.linalg
 from stats import betweenClassCovariance, withinClassCovariance
 from dataset import load_db, NUM_CLASSES
 
+'''
+LDA:
+	K = Number of classes
+	m = Number of attributes (directions)
+'''
 
 def LDA(D, L, K, m):
-	S_B = betweenClassCovariance(D, L, K)
-	S_W = withinClassCovariance(D, L, K)
+    if m > K-1:
+        exit(-1)
 
-	# generalized eigenvalue problem
-	s, U = scipy.linalg.eigh(S_B, S_W)
-	W = U[:, ::-1][:, 0:m]
+    S_B = betweenClassCovariance(D, L, K)
+    S_W = withinClassCovariance(D, L, K)
 
-	#UW, _, _ = np.linalg.svd(W)
-	#U = UW[:, 0:m]
+    # Generalized eigenvalue problem
+    s, U = scipy.linalg.eigh(S_B, S_W)
+    W = U[:, ::-1][:, 0:m]
 
-	# project every point to m-dimensional subspace
-	DP = np.dot(W.T, D)     # dim = mxN
-	return DP
+    # Find orthogonal basis of W
+    UW, _, _ = np.linalg.svd(W)
+    U = UW[:, 0:m]
 
+    # Project every point to m-dimensional subspace
+    DP = np.dot(U.T, D)     # dim = mxN
 
-def LDA_all(D, L, K):		# K = number of classes
-    listDP = []
-    for m in range(1, K):	# compute LDA for every direction m < K
-        DP_m = LDA(D, L, K, m)
-        listDP.append(DP_m)
-    
-    return listDP
+    return DP
 
 
 def main():
     D, L = load_db()
 
-    # LDA: computing and plotting all DP for every m (< K)
-    listDP = LDA_all(D, L, NUM_CLASSES)
+    # LDA: computing LDA for every m (< K) -> binary problem: only 1 discriminant direction
+    DP = LDA(D, L, NUM_CLASSES, 1)
+
 
 if __name__ == "__main__":
     main()
