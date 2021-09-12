58bf42517b60cfbc4927c95648aed1b4a9ade938
Giorgio Voto <s288179@studenti.polito.it>
2021-09-12 18:48:50 +0200

    Added Polinomial SVM

--- src/mainSVM_POLY.py
+++ src/mainSVM_POLY.py
@@ -29,7 +29,7 @@ def compute_LLR_LTE(D, L, C, kf, gauss=False):
         LT = np.concatenate(L_SETS[:i] + L_SETS[i + 1:], axis=0)
         DE = D_SETS[i]
 
-        SVMObj = SVM.SVMKernClass(DT, LT, C, 1, kf, pt=0.5) #0.5
+        SVMObj = SVM.SVMKernClass(DT, LT, C, 1, kf) #0.5
         x0 = np.zeros(LT.size)
         SVMObj.computeResult(x0)
         S_i, _ = SVMObj.computeScore(DE)
@@ -62,40 +62,36 @@ def main_find_best_C(kf, gauss=False):
     plt.ylabel("Min DCF")
     plt.show()
 
-def main_find_best_C_and_L(gauss=False):
+def main_find_best_C(gauss=False):
     D, L = db.load_db()
     Csub = np.logspace(-3, 3, 10)
     N = Csub.size
-    kf1 = SVM.RBF_F(np.exp(-1), 1.0)
-    kf2 = SVM.RBF_F(np.exp(-2), 1.0)
-    kf3 = SVM.RBF_F(np.exp(-3), 1.0)
+    kf1 = SVM.POLY_F(2.0, 0.0, 1.0)
     minDCF1 = np.zeros(N)
-    minDCF2 = np.zeros(N)
-    minDCF3 = np.zeros(N)
+    minDCF5 = np.zeros(N)
+    minDCF9 = np.zeros(N)
 
     i = 0
     for C in Csub:
         LLR, LTE = compute_LLR_LTE(D, L, C, kf1, gauss=gauss)
-        minDCF1[i] = computeDCFMin(LLR, LTE, 0.5)
-        LLR, LTE = compute_LLR_LTE(D, L, C, kf2, gauss=gauss)
-        minDCF2[i] = computeDCFMin(LLR, LTE, 0.5)
-        LLR, LTE = compute_LLR_LTE(D, L, C, kf3, gauss=gauss)
-        minDCF3[i] = computeDCFMin(LLR, LTE, 0.5)
+        minDCF1[i] = computeDCFMin(LLR, LTE, 0.1)
+        minDCF5[i] = computeDCFMin(LLR, LTE, 0.5)
+        minDCF9[i] = computeDCFMin(LLR, LTE, 0.9)
         i = i + 1
 
     plt.figure()
-    plt.semilogx(Csub, minDCF1, label='log(λ)=-1')
-    plt.semilogx(Csub, minDCF2, label='log(λ)=-2')
-    plt.semilogx(Csub, minDCF3, label='log(λ)=-3')
+    plt.semilogx(Csub, minDCF1, label='mindcf (π=0.1)')
+    plt.semilogx(Csub, minDCF5, label='mindcf (π=0.5)')
+    plt.semilogx(Csub, minDCF9, label='mindcf (π=0.9)')
     plt.legend()
     plt.xlabel("C")
     plt.ylabel("Min DCF")
     plt.show()
 
-def main_print_DCFMin(C, kf, gauss=False):
+def main_print_DCFMin(C, gauss=False):
     D, L = db.load_db()
     Csub = np.logspace(-3, 3, 10)
-    N = Csub.size
+    kf = SVM.POLY_F(2.0, 0.0, 1.0)
 
     LLR, LTE = compute_LLR_LTE(D, L, C, kf, gauss=gauss)
     print("πtrain 0.1: ",computeDCFMin(LLR, LTE, 0.1))
@@ -110,14 +106,8 @@ def main_print_show_Bayes(C, kf, gauss=False):
     plt.show()
 
 if __name__ == "__main__":
-    gauss=False
-    #main_find_best_C_and_L(gauss)
-    gauss = True
-    kf = SVM.RBF_F(np.exp(-1), 1.0)
-    C=2
-
-    #gauss = False
-    #kf = SVM.RBF_F(np.exp(-3), 1.0)
-    #C=10
-    #main_print_DCFMin(C, kf, gauss)
-    main_print_show_Bayes(C, kf, gauss)
+    gauss=True
+    main_find_best_C(gauss)
+    #C = 1
+    #main_print_DCFMin(C, gauss)
+    #main_print_show_Bayes(C, kf, gauss)
